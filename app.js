5f9600b6b637f1bf5c5923f7aa3ee110853b9442
Michael Weller <vve11ermichae1@gmail.com>
2019-03-02 12:22:52 +0000

    Added if statement to check if port is a parameter

--- app.js
+++ app.js
@@ -3,13 +3,13 @@ var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var postRoutes = require('./routes/postRoutes');
 var userRoutes = require('./routes/userRoutes');
-
 var morgan = require('morgan');
 var cookieParser = require('cookie-parser');
 
 // Configuration 
 var app = express(); 
 // var url = 'mongodb://localhost:27017/postin';
+// var url = "mongodb://admin:password1@ds243931.mlab.com:43931/heroku_7qv0mt3n"
 var url = process.env.MONGODB_URI;
 mongoose.connect(url, { useNewUrlParser: true});
 
@@ -20,9 +20,19 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
-app.use(express.static('public'));
-app.set('port', process.env.PORT || 3001);
 
+// For Heroku
+// If port parameter is set then set port to it otherwise use process.env.PORT 
+if (process.argv[2]) {
+    app.set('port', process.argv[2]);
+    var port = process.argv[2];
+}
+else {
+    app.set('port', process.env.PORT);
+    var port = process.env.PORT;
+}
+
+app.use(express.static('public'));
 
 // Routes 
 app.use('/', postRoutes);
@@ -33,7 +43,7 @@ app.get('/', function (req, res) {
 })
 
 mongoose.connection.once('open', () => {
-    app.listen( process.env.PORT, function() {
+    app.listen( port, function() {
         console.log('Server started!');
     });
 })
