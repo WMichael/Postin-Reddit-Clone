135fbf3d905c049bc49efbe878514db2da563293
Michael Weller <vve11ermichae1@gmail.com>
2019-03-03 14:26:47 +0000

    Redirect to posts instead of profile

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -11,7 +11,7 @@ module.exports = (router,passport,isLoggedIn) => {
 
     // Login Post 
     router.post('/login', passport.authenticate('local-login', {
-        successRedirect : '/profile', // redirect to the secure profile section
+        successRedirect : '/', // redirect to the secure profile section
         failureRedirect : '/login', // redirect back to the signup page if there is an error
         failureFlash : true // allow flash messages
     }));
